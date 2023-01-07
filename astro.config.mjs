04ba94e1533c2e42f7eda789324a179396d2b130
Harry Vince <harryavince@gmail.com>
2023-01-07 16:18:17 +0000

    added adapters required for build

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,16 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 import vue from "@astrojs/vue";
 
 // https://astro.build/config
+import node from "@astrojs/node";
+
+// https://astro.build/config
 export default defineConfig({
   integrations: [vue()],
-  output: 'server',
+  output: "server",
+  adapter: node({
+    mode: "standalone",
+  }),
 });
