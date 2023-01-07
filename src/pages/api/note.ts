265a0e06edceee6984e6b2119eda393a0bb7d4ca
Harry Vince <harryavince@gmail.com>
2023-01-07 14:12:44 +0000

    created api routes to view and create notes

---
+++ src/pages/api/note.ts
@@ -0,0 +1,20 @@
+import type { APIRoute } from "astro";
+import { ApiResponse } from "../../utils/api-helper";
+import { prisma } from "../../server/db/client";
+import { API_BAD_BODY_PROVIDED, API_INTERNAL_SERVER_ERROR, API_NO_BODY_PROVIDED, HTTP_BAD_REQUEST, HTTP_STATUS_CREATED, HTTP_STATUS_INTERNAL_SERVER_ERROR } from "../../utils/constants";
+
+export const get: APIRoute = async ({ request }) => {
+    const notes = await prisma.note.findMany();
+    return ApiResponse(notes, 200);
+}
+
+export const post: APIRoute = async ({ request }) => {
+    const { Title, Data } = await request?.json();
+    if (!Title || !Data) return ApiResponse(API_BAD_BODY_PROVIDED, HTTP_BAD_REQUEST);
+
+    const note = await prisma.note.create({
+        data: { Title, Data }
+    }).catch(() => { return ApiResponse(API_INTERNAL_SERVER_ERROR, HTTP_STATUS_INTERNAL_SERVER_ERROR) });
+
+    return ApiResponse({ note }, HTTP_STATUS_CREATED);
+} 
---
+++ src/utils/api-helper.ts
@@ -0,0 +1,11 @@
+import { API_NO_BODY_PROVIDED, HTTP_BAD_REQUEST } from "./constants";
+
+/**
+ * Default required response for api routes in astro.
+ * @param {object} object Javascript / Json to return in API Response.
+ * @param {number} number HTTP Repsonse code to return in API Response.
+ * @returns {Response} returns a JSON & HTTP response for use in the Astro api routes
+ */
+export const ApiResponse = (object: any, statusCode: number): Response => {
+    return new Response(JSON.stringify(object), { status: statusCode })
+}
